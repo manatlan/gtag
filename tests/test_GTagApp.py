633788d9541e5a97fffc4cfef32815da27093e6c
manatlan <manatlan@gmail.com>
2020-06-02 22:55:08 +0200

    just tu

--- tests/test_GTagApp.py
+++ tests/test_GTagApp.py
@@ -1,34 +1,37 @@
 from gtag import GTag,Tag
+import pytest
 
-# def test_GTagApp_run():
-#     class My(GTag):
-#         size=(100,100)
-#         def build(self):
-#             return Div("hello")
-#         def script(self): # just present to close the app after run
-#             return self.bind.evtExit(42) # produce js (bindUpdate)
-#         def evtExit(self,p):
-#             self.exit(p)
+@pytest.mark.skip(reason="could coz trouble with vscode <> chrome")
+def test_GTagApp_run():
+    class My(GTag):
+        size=(100,100)
+        def build(self):
+            return Tag.div("hello")
+        def script(self): # just present to close the app after run
+            return self.bind.evtExit(42) # produce js (bindUpdate)
+        def evtExit(self,p):
+            self.exit(p)
 
 
-#     m=My()
-#     assert isinstance(m._tag,Tag)
-#     assert ">hello<" in str(m)
-#     assert m.run()==42
+    m=My()
+    assert isinstance(m._tag,Tag)
+    assert ">hello<" in str(m)
+    assert m.run()==42
 
-# def test_GTagApp_serve():
-#     class My(GTag):
-#         size=(100,100)
-#         def build(self):
-#             return Div("hello")
-#         def script(self): # just present to close the app after run
-#             return self.bind.evtExit(42) # produce js (bindUpdate)
-#         def evtExit(self,p):
-#             self.exit(p)
+@pytest.mark.skip(reason="could coz trouble with vscode <> chrome")
+def test_GTagApp_serve():
+    class My(GTag):
+        size=(100,100)
+        def build(self):
+            return Tag.div("hello")
+        def script(self): # just present to close the app after run
+            return self.bind.evtExit(42) # produce js (bindUpdate)
+        def evtExit(self,p):
+            self.exit(p)
 
-#     m=My()
-#     assert isinstance(m._tag,Tag)
-#     assert ">hello<" in str(m)
-#     assert m.serve()==42
+    m=My()
+    assert isinstance(m._tag,Tag)
+    assert ">hello<" in str(m)
+    assert m.serve()==42
 
 
