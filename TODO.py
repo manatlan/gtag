fba8dde16ceb024795b44d04d6f11d1559254769
manatlan <manatlan@gmail.com>
2020-06-08 08:11:44 +0200

    better todo

--- TODO.py
+++ TODO.py
@@ -13,6 +13,7 @@ class Comp1(GTag):
             Tag.div( "self=", self.value ),
             Tag.button( "++", onclick=self.bind.clickMe(1) ),
         )
+    # @local
     def clickMe(self,inc):
         self.value+=inc
 
@@ -26,24 +27,28 @@ class Comp2(GTag):
             Tag.div( "self=", self.value ),
             Tag.button( "++", onclick=self.bind.clickMe(1) ),
         )
+    # @local
     def clickMe(self,inc):
         self.value+=inc
 
 class App(GTag):
-    size=(200,200)
+    size=(250,250)
     def init(self):
         self.cpt1=dict(init=0,build=0)
         self.cpt2=dict(init=0,build=0)
 
-        self.v1=42
-        self.c2=Comp2(42)
+        self.v1=0
+        self.c2=Comp2(0)
     def build(self): # build will rebuild innerchilds too (without that : trouble !!!)
         return Tag.div(
-            Comp1(self.bind.v1),"exported=",self.v1,              # real child
-            self.cpt1,
-            "<hr>",
-            self.c2,"exported=",self.c2.value,                   # innerchild
-            self.cpt2,
+
+            "REAL CHILD",
+            Comp1(self.bind.v1),"v=",self.v1,
+            "<br>",self.cpt1,
+
+            "<hr> INNERCHILD",
+            self.c2,"v=",self.c2.value,
+            "<br>",self.cpt2,
         )
 
 app=App()
