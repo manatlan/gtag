43769bcda742676926ee573c46821a9dda76b4f8
manatlan <manatlan@gmail.com>
2020-06-25 18:43:40 +0200

    r0.0.27 : better

--- gtag/__init__.py
+++ gtag/__init__.py
@@ -16,7 +16,7 @@
 #    more: https://github.com/manatlan/guy
 # #############################################################################
 
-__version__="0.0.26"
+__version__="0.0.27"
 
 from .gtag import GTag, Tag, value, render
 
